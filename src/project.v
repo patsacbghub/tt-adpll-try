250b59feec3b2341ee9ef62f1d3415c609f2fd05
patsacbghub <80314686+patsacbghub@users.noreply.github.com>
2024-12-05 13:50:29 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_dpetrisko_ttdll (
+module tt_um_patsacbghub_apdll (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
